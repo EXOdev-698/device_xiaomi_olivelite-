7403a8c5c529f8a593eae138baaf0a5ea7ac94f4
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 20:16:49 +0200

    olivelite: overlay-lineage: Device have notch
    Change-Id: Ib912728020f85c820b10d5102365dc37f9a54342
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Inherit from sdm439-common
 $(call inherit-product, device/xiaomi/sdm439-common/sdm439.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
